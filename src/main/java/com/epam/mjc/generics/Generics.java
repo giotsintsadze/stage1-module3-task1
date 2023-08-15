79ad74d1a7b0f57cc5e8a76808f971f5a8ca6186
Giorgi Tsintsadze <giorgi.tsintsadze@scnadiweb.com>
2023-08-15 13:35:12 +0400

    Generic task

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -5,23 +5,36 @@ import java.util.List;
 
 public class Generics {
 
-    //TODO: Refactor Method-1
-    public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
+    // Refactor Method-1
+    public List<String> boxingMethod(String name) {
+        List<String> firstList = new ArrayList<>();
         firstList.add(name);
-        List secondList = new ArrayList<>();
-        secondList.add(firstList);
-        return secondList;
+        return firstList;
     }
 
-    //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+
+    // Refactor Method-2
+    public <T> T genericMethod(T data) {
         return data;
     }
 
-    //TODO: Refactor Method-3
-    public void cloneMethod(List consumer, List producer) {
+    // Refactor Method-3
+    public <T> void cloneMethod(List<T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 
+    public static void main(String[] args) {
+        Generics generics = new Generics();
+
+        List<String> firstList = generics.boxingMethod("Hello");
+
+        String value = generics.genericMethod("Generic Data");
+
+        List<Integer> producerList = new ArrayList<>();
+        producerList.add(1);
+        producerList.add(2);
+
+        List<Integer> consumerList = new ArrayList<>();
+        generics.cloneMethod(consumerList, producerList);
+    }
 }
